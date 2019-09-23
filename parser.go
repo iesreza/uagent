16c6d89cb8a4ea6d9a825df12e11b490ef922e46
Reza <reza@ies-italia.it>
2019-09-23 19:42:59 +0330

    Update

--- parser.go
+++ parser.go
@@ -1,13 +1,13 @@
 package uagent
 
-import "uagent/classifiers"
+import "github.com/iesreza/uagent/classifiers"
 
 func Parse(ua string) classifiers.Describer {
 	describer := classifiers.Describer{}
-	for _,cList := range classifiers.Classifiers{
-		if len(cList) > 0{
-			for _,classifier := range cList{
-				if classifier.Eval(ua,&describer){
+	for _, cList := range classifiers.Classifiers {
+		if len(cList) > 0 {
+			for _, classifier := range cList {
+				if classifier.Eval(ua, &describer) {
 					break
 				}
 			}
